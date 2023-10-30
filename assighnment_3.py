a6fd085abe133c3b9184af21958f7e5c0cd42480
Baraa Fawaz <baraafawaz94@gmail.com>
2023-10-30 14:31:49 +0200

    second function

--- assighnment_3.py
+++ assighnment_3.py
@@ -43,3 +43,32 @@ def addMatrices(mat_1, mat_2):
 
     print("The result of matrix addition is:")
     print(result)
+def checkRotation(m1, m2):
+    #take the two functions from the user in the same way use at the first function
+    row_1 = eval(input("Enter number of rows of the first matrix: "))
+    column_1 = eval(input("Enter number of columns of the first matrix : "))
+    mat_1 = []
+    for i in range(row_1):
+        mat_1.append([])
+    for i in range(row_1):
+        for j in range(column_1):
+            mat_1[i].append(j)
+    for i in range(row_1):
+        for j in range(column_1):
+            print("enter the value in row", i + 1, "col", j + 1)
+            mat_1[i][j] = int(input())
+    print("you first matrix is", mat_1)
+    row_2 = eval(input("Enter number of rows of the second matrix: "))
+    column_2 = eval(input("Enter number of columns of the second matrix : "))
+    mat_2 = []
+    for i in range(row_2):
+        mat_2.append([])
+    for i in range(row_2):
+        for j in range(column_2):
+            mat_2[i].append(j)
+            mat_2[i][j] = 0
+    for i in range(row_2):
+        for j in range(column_2):
+            print("enter the value in row", i + 1, "col", j + 1)
+            mat_2[i][j] = int(input())
+    print("you second matrix is", mat_2)
