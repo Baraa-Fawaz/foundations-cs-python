92b04253f7b4049ccf28119c4b2285b613ec10a6
Baraa Fawaz <baraafawaz94@gmail.com>
2023-11-12 17:42:20 +0200

    main function

--- FCS Midterm solution/Midterm_solution.py
+++ FCS Midterm solution/Midterm_solution.py
@@ -22,3 +22,34 @@ def displayMenu():
   print("1. Open Tab\n2. Close Tab\n3. Switch Tab")
   print("4. Display All Tabs\n5. Open Nested Tab\n6. Clear All Tabs")
   print("7. Save Tabs\n8. Import Tabs\n9. Exit")  #using 3 print statment only for better visualization
+def main():
+  displayMenu()
+  choice = eval(input("Enter a number: "))
+
+  while choice != 9:
+    if choice == 1:
+      openTab()
+    elif choice == 2:
+      closeTab()
+    elif choice == 3:
+      switchTab()
+    elif choice == 4:
+      displayAllTabs()
+    elif choice == 5:
+      openNestedTab()
+    elif choice == 6:
+      clearAllTabs()
+    elif choice == 7:
+      saveTabs()
+    elif choice == 8:
+      importTabs()
+    elif choice != 9:
+      print("Invalid input.Try to choose a number from 1 to 9")
+
+    displayMenu()
+    choice = eval(input("Enter a number: "))
+
+  print("Exited")  #if choice = 9 the while loop will be entered thus exiting and terminating the program.
+
+
+main()
