8e6b9deb3f6072d36381f49ff0f0bcaf8c11dda4
Baraa Fawaz <baraafawaz94@gmail.com>
2023-11-12 17:34:35 +0200

    initilizing the functions

--- FCS Midterm solution/Midterm_solution.py
+++ FCS Midterm solution/Midterm_solution.py
@@ -1,3 +1,20 @@
 #Greeting the user
 user_name = input("Enter your name:")
 print("Hello", user_name, "Welcomt to the Advanced Browser Tabs Simulation")
+
+def openTab():
+    pass
+def closeTab():
+    pass
+def switchTab():
+    pass
+def displayAllTabs():
+    pass
+def openNestedTab():
+    pass
+def clearAllTabs():
+    pass
+def saveTabs():
+    pass
+def importTabs():
+    pass
