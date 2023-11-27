a207a8060edc705a2ae04995329007e51e8ef1a6
Baraa Fawaz <baraafawaz94@gmail.com>
2023-11-27 16:57:04 +0200

    final code

--- assighnment_4.py
+++ assighnment_4.py
@@ -282,7 +282,7 @@ class Graph:#Worst case ---> O(V)
     
 def displayMainMenu():#Worst case ---> O(1)
     print("1. Singly Linked List\n2. Check if Palindrome\n3. Priority Queue\n4. Evaluate an Infix Expression\n5. Graph\n6. Exit")
-def main():# O(N^2)
+def main():#worst case ---> O(N^2)
     sll = SinglyLinkedList()
     pq = PriorityQueue()
     graph = Graph()
